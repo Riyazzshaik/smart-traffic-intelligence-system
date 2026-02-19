5c5b42a2374c5ebe320bc635b02009145bb40137
Riyaz shaik <mr.riyazshaik143@gmail.com>
2026-02-19 18:42:50 +0530

    Connected frontend to live backend

--- frontend/src/components/RiskDashboard.js
+++ frontend/src/components/RiskDashboard.js
@@ -53,7 +53,7 @@ const RiskDashboard = () => {
                 }
             }
 
-            const response = await fetch("http://127.0.0.1:5000/predict", {
+            const response = await fetch("https://smart-traffic-api-u09k.onrender.com/predict", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(formData)
